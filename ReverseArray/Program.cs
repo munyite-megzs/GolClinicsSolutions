1e8afcf5d06b02b92bc991aec7184c4365e8e851
munyite-megzs <margaret.sitati1@gmail.com>
2021-05-18 09:39:30 +0300

    Reversed Array Solution

---
+++ ReverseArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[] { 10,5,6,9};
+            int count = 0;
+            int length = A.Length-1;
+
+            while (count < length)
+            {
+                var temp = A[count];
+                A[count] = A[length];
+                A[length] = temp;
+
+                count++;
+                length--;
+
+            }
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
